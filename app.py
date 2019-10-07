619866cd0f850f451a373bfd2fd25b86211267ec
gexty444 <ashlyn444@hotmail.com>
2019-10-07 21:51:32 +0800

    added logging feature

--- app.py
+++ app.py
@@ -2,10 +2,36 @@ from flask import Flask
 from flask_restful import Api
 from resources.foo import Foo
 import common.util
+import datetime
+import logging
 
 app = Flask(__name__)
 api = Api(app)
+logging.basicConfig(level=logging.DEBUG,
+                    format="%(asctime)s %(levelname)s %(name)s %(threadName)s : %(message)s")
 
+@app.route('/hello', methods=['GET'])
+def hello():
+	return {'greeting': 'have a nice day'}
 
+@app.route('/hello/<name>', methods=['GET'])
+def greet(name):
+	return {'greeting': "Hello {}. Have a nice day".format(name)}
 
-    
+@app.after_request
+def log_request(response):
+    time = datetime.datetime.now()
+    body = response.data
+    status_as_string = response.status
+    status_as_integer = response.status_code
+    try:
+        _id = common.util.mongo.db.logs.insert({
+            "time": time,
+            "body": body,
+            "status": status_as_string,
+            "status_code": status_as_integer
+        })
+        app.logger.debug("Successful insert with _id %s" % _id)
+    except:
+        app.logger.warning("Error encountered during insertion of log")    
+    return response
--- common/util.py
+++ common/util.py
@@ -3,4 +3,5 @@ from flask_pymongo import PyMongo
 
 app = Flask(__name__)
 app.config["MONGO_URI"] = "mongodb://localhost:27017/isit_database_mongo"
+app.config['LOG_MONGO'] = "mongodb://localhost:27017/log_mongo"
 mongo = PyMongo(app)
