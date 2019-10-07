92d04d98f80bf65921a4951bc0c5906fda3b6c83
pencilleaf <x-clairetanjq@hotmail.com>
2019-10-08 00:09:13 +0800

    Test mysql and mongo connection

--- common/util.py
+++ common/util.py
@@ -1,6 +1,13 @@
 from flask import Flask
 from flask_pymongo import PyMongo
+import mysql.connector as db
+
+DATABASE = "isit_database"
+HOST = "localhost"
 
 app = Flask(__name__)
 app.config["MONGO_URI"] = "mongodb://localhost:27017/isit_database_mongo"
 mongo = PyMongo(app)
+
+con = db.connect(host=HOST, user="root", passwd="root", db=DATABASE)
+cursor = con.cursor()
--- resources/foo.py
+++ resources/foo.py
@@ -1,8 +1,17 @@
 from flask import json
 from flask_restful import Resource
-from common.util import mongo
+from common.util import mongo, cursor
 
-class Foo(Resource):
+from bson import json_util
+import json
+
+class testMongo(Resource):
     def get(self):
-        text = mongo.db.logs.find_one({"complaint": "Why are you gay"})["complaint"]
-        return text
+        obj = mongo.db.kindle_metadata.find_one({"asin": "B000MAHACE"})
+        return json.dumps(obj, default=json_util.default)
+
+class testMySql(Resource):
+    def get(self):
+        cursor.execute("describe kindle_reviews")
+        res = cursor.fetchall()
+        return res
