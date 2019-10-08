9573de5bdcd86d1575d7b366df55b3cc3a0c5c86
Andre Hadianto <andre_hadianto@mymial.sutd.edu.sg>
2019-10-08 12:35:50 +0800

    setting up react base

---
+++ static/js/components/app.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+class App extends React.Component {
+    render() {
+        return (
+            <div className="test">
+                <p>Hello!</p>
+            </div>
+        )
+    }
+}
+
+export default App;
--- static/js/index.jsx
+++ static/js/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './app';
+import App from './components/app';
+import './styles/styles.scss';
 
-ReactDOM.render(<App/>, document.getElementById("content"));
+ReactDOM.render(<App/>, document.getElementById("app"));
