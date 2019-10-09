33c32edeeb754cf1cc184aa7f3a435debf8d2acc
celine <celine_yee@kelaberetiv.sg>
2019-10-09 15:18:06 +0800

    defined after_request function to fix

--- server/app.py
+++ server/app.py
@@ -19,6 +19,10 @@ api.add_resource(testMongo, '/mongo')
 
 # Invoked after every requests to log the timestamp, content & status
 @app.after_request
+def after_request(response):
+    response.direct_passthrough = False
+    return response
+
 def log_request(response):
     time = datetime.datetime.now()
     body = response.data.decode("utf-8")
