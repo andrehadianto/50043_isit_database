248834de8e9fd2c666703b21b7d5b4ee63426b50
Andre Hadianto <andre_hadianto@mymial.sutd.edu.sg>
2019-10-09 17:56:10 +0800

    replace antd with semantic-ui-react

--- static/src/components/home.jsx
+++ static/src/components/home.jsx
@@ -1,24 +1,43 @@
 import React, { Component } from 'react';
-import { Layout, Button } from 'antd';
-
-const { Header, Content, Footer } = Layout;
+import {
+    Menu,
+    Container,
+    Image,
+    Dropdown,
+} from 'semantic-ui-react';
 
 class Home extends Component {
     render() {
         return (
-            <Layout>
-                <Header className="header">
-                    <Button>Test</Button>
-                </Header>
-                <Content>
-                    <div className="home">
-                        <p>Content</p>
-                    </div>
-                </Content>
-                <Footer>
-                    <p>footer</p>
-                </Footer>
-            </Layout>
+            <div>
+                <Menu fixed='top' inverted>
+                    <Container>
+                        <Menu.Item as='a' header>
+                        <Image size='mini' style={{ marginRight: '1.5em' }} />
+                        Project Name
+                        </Menu.Item>
+                        <Menu.Item as='a'>Home</Menu.Item>
+
+                        <Dropdown item simple text='Dropdown'>
+                        <Dropdown.Menu>
+                            <Dropdown.Item>List Item</Dropdown.Item>
+                            <Dropdown.Item>List Item</Dropdown.Item>
+                            <Dropdown.Divider />
+                            <Dropdown.Header>Header Item</Dropdown.Header>
+                            <Dropdown.Item>
+                            <i className='dropdown icon' />
+                            <span className='text'>Submenu</span>
+                            <Dropdown.Menu>
+                                <Dropdown.Item>List Item</Dropdown.Item>
+                                <Dropdown.Item>List Item</Dropdown.Item>
+                            </Dropdown.Menu>
+                            </Dropdown.Item>
+                            <Dropdown.Item>List Item</Dropdown.Item>
+                        </Dropdown.Menu>
+                        </Dropdown>
+                    </Container>
+                </Menu>
+            </div>
         )
     }
 }
