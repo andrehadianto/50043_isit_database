f7d9281b988bb947bdf302ec7bfa90fe4d417b75
Chong Yi Cheng <yicheng_chong@mymail.sutd.edu.sg>
2019-10-14 14:34:10 -0400

    - changed obtaining the information to obtaining from req form - renamed file to book_review.py

--- server/resources/book_preview.py
+++ server/resources/book_preview.py
@@ -9,24 +9,6 @@ from bson.json_util import dumps
 from bson import json_util
 import json
 
-class MetaAPI(Resource):
-    """Returns books information (lightweight) with pagination"""
-    def get(self):
-        parser = reqparse.RequestParser()
-        parser.add_argument('page', type=int, location='args')
-        parser.add_argument('count', type=int, location='args')
-        args = parser.parse_args()
-        
-        _limit = args['count']
-        _offset = (args['page']-1) * args['count']
-        
-        # Returns only fields asin and imUrl
-        cursor = mongo.db.kindle_metadata.find({},
-             {"asin" : 1, "imUrl" : 1}).skip(_offset).limit(_limit)
-        jsonstring = json_util.dumps(cursor, default=json_util.default)
-        return json.loads(jsonstring)
-
-
 class BookPreviewResource(Resource):
     
     def post(self):
@@ -34,7 +16,7 @@ class BookPreviewResource(Resource):
         # asinArray: array of string
         # Response Body: Array of json(asin, title, imURL?)
         parser = reqparse.RequestParser()
-        parser.add_argument('asinArray', type=str, location='args')
+        parser.add_argument('asinArray', type=str, location='form')
         args = parser.parse_args()
         asinArray = args.get('asinArray').split(",")
         print(asinArray)
