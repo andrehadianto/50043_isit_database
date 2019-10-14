955cd88436bd2405f8274a8ab1526ec34455fbc7
Chong Yi Cheng <yicheng_chong@mymail.sutd.edu.sg>
2019-10-14 10:26:14 -0400

    - Fixed /books/preview. Post works when array of asin is passed as args

--- server/resources/meta.py
+++ server/resources/meta.py
@@ -4,6 +4,7 @@ from flask_restful import Resource, reqparse
 from flask_restful import Resource
 from common.util import mongo, cursor
 
+
 from bson.json_util import dumps
 from bson import json_util
 import json
@@ -33,23 +34,37 @@ class BookPreviewResource(Resource):
         # asinArray: array of string
         # Response Body: Array of json(asin, title, imURL?)
         parser = reqparse.RequestParser()
-        parser.add_argument('asinArray', type=str, location='form')
+        parser.add_argument('asinArray', type=str, location='args')
         args = parser.parse_args()
-        asinArray = args.get('value')
+        asinArray = args.get('asinArray').split(",")
         print(asinArray)
-
-        #print(cursor)
-        #print("jsonString:",jsonstring)
-        #check if bookInfoInJSON is empty?
-        #bookInfoInJSON.append(booksJSONArray)
-          
-        #print("BOOKS JSON ARRAY: ", booksJSONArray)
-        #return booksJSONArray
         # create an array to host the json
-        # For each asin in asinArray, parse and request for the asin and its relevant info
-        # If don't exist, put some placeholder
-        # host each asin and its info into the array as a json
-        # 
+        booksJSONArray = list()
+        
+            # For each asin in asinArray, parse and request for the asin and its relevant info
+        for asin in asinArray:
+            bookInfo = mongo.db.isit_database.find_one({"asin":asin})
+            print("bookInfo:" ,bookInfo)
+
+            book_asin = asin
+            
+            try:
+                book_title = bookInfo["title"]
+            except Exception as e:
+                book_title = "untitled"
+                
+            try:
+                book_imUrl = bookInfo["imUrl"]
+            except Exception as e:
+                book_imUrl = bookInfo[""]
+              
+            bookLW = {"asin": book_asin, "title": book_title, "imUrl":book_imUrl}
+            booksJSONArray.append(json.dumps(bookLW))
+        
+        return booksJSONArray
+
+
+    
 
 class BookCategoryResource(Resource):
     
