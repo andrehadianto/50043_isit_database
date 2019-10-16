6dcfd73b732311c1a66a66f3c947d3f2bcc16abd
Andre Hadianto <andre_hadianto@mymial.sutd.edu.sg>
2019-10-16 09:55:38 +0800

    Add price in book details, add login template

--- static/src/components/NavBar.jsx
+++ static/src/components/NavBar.jsx
@@ -6,7 +6,12 @@ import {
     Menu,
     Image,
     Button,
-    Icon
+    Icon,
+    Modal,
+    Header,
+    Form,
+    Message,
+    Grid
 } from 'semantic-ui-react';
 
 const { Item } = Menu;
@@ -17,15 +22,49 @@ const NavBar = () => (
             <Container>
                 <Link to='/'>
                     <Item header>
-                        <Image size='mini' src="https://icon-library.net/images/react-icon/react-icon-28.jpg" style={{ marginRight: '1.5em' }} />
+                        <Image size='mini' src='https://icon-library.net/images/react-icon/react-icon-28.jpg' style={{ marginRight: '1.5em' }} />
                         isit database?
                     </Item>
                 </Link>
                 <Item position='right'> 
-                    <Button icon as='a'inverted labelPosition='left'>
-                        <Icon inverted name='sign in'/>
-                        Log in
-                    </Button>
+                    <Modal
+                        trigger={
+                            <Button icon as='a'inverted labelPosition='left'>
+                                <Icon inverted name='sign in'/>
+                                Log in
+                            </Button>}
+                        style={{ maxWidth: '450px' }}
+                    >
+                        <Modal.Content>
+                                <Grid textAlign='center' verticalAlign='middle'>
+                                    <Grid.Column>
+                                        <Header as='h2' color='blue' textAlign='center'>
+                                            <Image src='https://icon-library.net/images/react-icon/react-icon-28.jpg' /> Log-in to your account
+                                        </Header>
+                                        <Form size='large'>
+                                            <Segment stacked>
+                                            <Form.Input fluid icon='user' iconPosition='left' placeholder='Username' />
+                                            <Form.Input
+                                                fluid
+                                                icon='lock'
+                                                iconPosition='left'
+                                                placeholder='Password'
+                                                type='password'
+                                            />
+
+                                            <Button color='blue' fluid size='large'>
+                                                Login
+                                            </Button>
+                                            </Segment>
+                                        </Form>
+                                        <Message>
+                                            New to us? <a href='#'>Sign Up</a>
+                                        </Message>
+                                    </Grid.Column>
+                            </Grid>
+                        </Modal.Content>
+                    </Modal>
+
                 </Item>
             </Container>
         </Menu>
