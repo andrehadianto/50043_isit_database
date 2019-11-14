80ac3205d4418c27801681a3feb96fd368268182
Andre Hadianto <andre_hadianto@mymial.sutd.edu.sg>
2019-11-14 11:21:45 +0800

    remove duplicates

--- static/src/pages/UserAction.jsx
+++ static/src/pages/UserAction.jsx
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
-import axios from 'axios';
-import AddNewBook from '../components/AddNewBook';
+import AddNewBook from '../pages/AddNewBook';
 import {
     Grid, Container
 } from 'semantic-ui-react';
