0800bf30d91ba001916ed0a46dcd7807f552a945
Andre Hadianto <andre_hadianto@mymial.sutd.edu.sg>
2019-12-04 11:48:23 +0800

    Added category label

--- static/src/components/FilteredBooks.jsx
+++ static/src/components/FilteredBooks.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, Fragment } from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
 import _ from 'lodash';
@@ -11,7 +11,8 @@ import {
     Divider,
     Container,
     Input,
-    Button
+    Button,
+    Label
 } from 'semantic-ui-react';
 
 const { Column } = Grid;
@@ -65,45 +66,56 @@ const FilteredBooks = (props) => {
     }
 
     return (
-        <Grid>
-            <Column width={16}>
-                    <Grid columns={6}>
-                    {
-                        isLoading
-                        ? preview_placeholder
-                        : bookData.map((book, index) => {
-                            return (
-                                <Column key={index}>
-                                    <Link to={{pathname: `/review/${book.asin}`}}>
-                                        <Item>
-                                            <Item.Image verticalAlign='middle' size='small' style={{minWidth: '150px', minHeight: '150px'}} src={book.imUrl}/>
-                                            <Header textAlign='center' as='h5'>{book.asin}</Header>
-                                        </Item>
-                                    </Link>
-                                </Column>
-                            )
-                        })
-                    } 
-                    </Grid>
-                    <Divider/>
-                    <Container textAlign='center'>
-                        <Pagination
-                            secondary
-                            ellipsisItem={null}
-                            activePage={ activePage }
-                            onPageChange={ onPageChange }
-                            totalPages={99}
-                        />
-                        <Input 
-                            onChange={ (e, {value}) => {setGoToPage(value)} }
-                            value={goToPage}
-                            style={{ width: '4em' }} 
-                            action={ <Button content='Go' onClick={ goToClickHandler }/> }
-                            error={isInvalid}
-                        />
-                    </Container>
-            </Column>
-        </Grid>
+        <Fragment>
+            <Grid>
+            {
+                props.location.state.filter.map((category, index) => {
+                    return (
+                        <Label content={ category } key={ index }/>
+                    )
+                })
+            }
+            </Grid>
+            <Grid>
+                <Column width={16}>
+                        <Grid columns={6}>
+                        {
+                            isLoading
+                            ? preview_placeholder
+                            : bookData.map((book, index) => {
+                                return (
+                                    <Column key={index}>
+                                        <Link to={{pathname: `/review/${book.asin}`}}>
+                                            <Item>
+                                                <Item.Image verticalAlign='middle' size='small' style={{minWidth: '150px', minHeight: '150px'}} src={book.imUrl}/>
+                                                <Header textAlign='center' as='h5'>{book.asin}</Header>
+                                            </Item>
+                                        </Link>
+                                    </Column>
+                                )
+                            })
+                        } 
+                        </Grid>
+                        <Divider/>
+                        <Container textAlign='center'>
+                            <Pagination
+                                secondary
+                                ellipsisItem={null}
+                                activePage={ activePage }
+                                onPageChange={ onPageChange }
+                                totalPages={99}
+                            />
+                            <Input 
+                                onChange={ (e, {value}) => {setGoToPage(value)} }
+                                value={goToPage}
+                                style={{ width: '4em' }} 
+                                action={ <Button content='Go' onClick={ goToClickHandler }/> }
+                                error={isInvalid}
+                            />
+                        </Container>
+                </Column>
+            </Grid>
+        </Fragment>
     );
     
 }
