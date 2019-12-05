9234e4183c3a76301ed818c53771f8b1934e53c4
pencilleaf <claire_tan@mymail.sutd.edu.sg>
2019-12-05 19:44:24 +0800

    Updated flask script

--- automation/scripts/flask_script.sh
+++ automation/scripts/flask_script.sh
@@ -10,7 +10,7 @@ nodejs -v
 npm -v
 
 # Download code
-wget -c https://www.dropbox.com/s/x6pcb278trb2hrn/50043_isit_database-master.zip?dl=0 -O 50043_isit_database.zip
+wget -c https://www.dropbox.com/s/9muw228zsm2z6wy/50043_isit_database-master2.zip?dl=0 -O 50043_isit_database.zip
 sudo apt install unzip
 unzip 50043_isit_database.zip
 
@@ -30,21 +30,3 @@ LOCAL_PUBLIC_IP=$(curl ifconfig.co)
 export LOCAL_PUBLIC_IP
 echo $LOCAL_PUBLIC_IP
 npx webpack --env.API_URL=http://$LOCAL_PUBLIC_IP:5000 --progress -p --mode=production --config webpack.config.js 
-
-# # Create .env 
-# # cd ../server
-
-# cat >> .env << EOF
-# SQL_DB=isit_database
-# SQL_USER=root
-# SQL_PW=password
-# SQL_HOST=$1
-# MONGO_DB=isit_database_mongo
-# LOG_DB=log_mongo
-# MONGO_HOST=$2
-# EOF
-
-# touch /home/ubuntu/script-finished.txt
-
-# # Run flask app
-# python app.py
