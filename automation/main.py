b0814a8aec569f476020251475296c9289c25871
pencilleaf <claire_tan@mymail.sutd.edu.sg>
2019-12-13 14:50:25 +0800

    Tidy up code

--- automation/main.py
+++ automation/main.py
@@ -22,12 +22,14 @@ import utils.user as user
 
 
 def main():
-    
+
+    # Set up variables
     LOCAL_IP = urllib.request.urlopen('http://ident.me').read().decode('utf8')
 
     AMAZON_LINUX_AMI = 'ami-05c859630889c79c8'
     UBUNTU_AMI = 'ami-061eb2b23f9f8839c'
     INSTANCE_TYPE = 't2.micro'
+    HADOOP_INSTANCE_TYPE='t2.medium'
 
     HADOOP_SCRIPT = os.path.join("scripts", "hadoop_script.sh")
     FLASK_SCRIPT = os.path.join("scripts", "flask_script.sh")
@@ -39,11 +41,17 @@ def main():
     CONFIG = dict()
     CONFIG["AWS_CREDENTIALS"] = {"ACCESS_KEY": user.ACCESS_KEY,"SECRET_KEY": user.SECRET_KEY, "KEY_PAIR": user.KEY_PAIR, "KEY_PATH": user.KEY_PATH}
 
+
     # Set up logging
     logging.basicConfig(level=logging.INFO,
                         format='%(levelname)s: %(asctime)s: %(message)s')
 
-    # Create security group
+
+    # *=================================================*
+    # *                     FLASK                       *
+    # *=================================================*
+
+    # Create security group for flask
     flask_permissions = [{'IpProtocol': 'tcp',
                             'FromPort': 22,
                             'ToPort': 22,
@@ -56,7 +64,9 @@ def main():
     flask_security_group = create_security_group("flask-webapp", flask_permissions)
     CONFIG["SECURITY_GROUPS"] = [flask_security_group]
 
-    # Provision and launch the EC2 instance
+    # Provision and launch the EC2 instance for flask
+    logging.info('Starting flask instance...')
+
     flask_instance_info = create_ec2_instance(1, UBUNTU_AMI, INSTANCE_TYPE, ["flask-webapp"], FLASK_SCRIPT)[0]
 
     if flask_instance_info is not None:
@@ -70,19 +80,24 @@ def main():
     CONFIG["FLASK"] = {"IP": flask_instance_info["PublicIpAddress"], "ID": flask_instance_info["InstanceId"]}
 
 
+    # *=================================================*
+    # *                     HADOOP                      *
+    # *=================================================*
 
-
-
+    # Create security group for hadoop/spark instances
     hadoop_permissions = [{'IpProtocol': 'tcp',
                             'FromPort': 0,
                             'ToPort': 65535,
                             'IpRanges': [{'CidrIp': '0.0.0.0/0'}]}]
 
     hadoop_security_group = create_security_group("hadoop", hadoop_permissions)
+
     CONFIG["SECURITY_GROUPS"].append(hadoop_security_group)
+
+    # Provision and launch the EC2 instances for hadoop/spark
     logging.info('Starting hadoop namenodes and datanodes...')
 
-    hadoop_instance_info = create_ec2_instance(int(user.NODES), UBUNTU_AMI, INSTANCE_TYPE, ["hadoop"], HADOOP_SCRIPT)
+    hadoop_instance_info = create_ec2_instance(int(user.NODES), UBUNTU_AMI, HADOOP_INSTANCE_TYPE, ["hadoop"], HADOOP_SCRIPT)
 
     if hadoop_instance_info is not None:
         for node in range(int(user.NODES)):
@@ -99,9 +114,11 @@ def main():
                                 "ID": hadoop_instance_info[i]["InstanceId"],
                                 "DNS": hadoop_instance_info[i]["PublicDnsName"]} for i in range(1, int(user.NODES))]
     
+    # *=================================================*
+    # *                     MYSQL                       *
+    # *=================================================*
 
-
-
+    # Create security group for mysql
     sql_permissions = [{'IpProtocol': 'tcp',
                             'FromPort': 22,
                             'ToPort': 22,
@@ -111,12 +128,13 @@ def main():
                             'ToPort': 3306,
                             'IpRanges': [{'CidrIp': CONFIG["FLASK"]["IP"] + '/32'},
                                         {'CidrIp': LOCAL_IP + '/32'},
-                                        {'CidrIp': CONFIG["MASTER"]["IP"] + '/32'}]}] # only allow flask to access mysql
+                                        {'CidrIp': CONFIG["MASTER"]["IP"] + '/32'}]}]
 
     sql_security_group = create_security_group("mysql-server", sql_permissions)
 
     CONFIG["SECURITY_GROUPS"].append(sql_security_group)
 
+    # Provision and launch the EC2 instance for mysql
     logging.info('Starting MySQL...')
 
     sql_instance_info = create_ec2_instance(1,AMAZON_LINUX_AMI, INSTANCE_TYPE, ["mysql-server"], SQL_SCRIPT)[0]
@@ -132,10 +150,11 @@ def main():
     CONFIG["MYSQL"] = {"IP": sql_instance_info["PublicIpAddress"], "ID": sql_instance_info["InstanceId"]}
     
 
-
-
-
-   
+    # *=================================================*
+    # *                     MONGO                       *
+    # *=================================================*
+    
+   # Create security group for mongo
     mongo_permissions = [{'IpProtocol': 'tcp',
                             'FromPort': 22,
                             'ToPort': 22,
@@ -151,6 +170,7 @@ def main():
 
     CONFIG["SECURITY_GROUPS"].append(mongo_security_group)
 
+    # Provision and launch EC2 instance for mongo
     logging.info('Starting MongoDB...')
 
     mongo_instance_info = create_ec2_instance(1, UBUNTU_AMI, INSTANCE_TYPE, ["mongo_db"], MONGO_SCRIPT)[0]
@@ -168,11 +188,16 @@ def main():
 
 
 
+    # Dump info to config.yml
     with open('config/config.yml', 'w') as file:
         documents = yaml.dump(CONFIG, file)
 
 
 
+
+    # *=================================================*
+    # *                 FLASK CONFIG                    *
+    # *=================================================*
 
     logging.info("Setting up the flask webapp...")
 
@@ -186,6 +211,7 @@ def main():
         else:
             break
 
+    # Create .env file for flask to connect to mongo/mysql
     cmds = [
         "sudo touch /50043_isit_database-master/server/.env && echo Created .env file",
         """sudo tee -a /50043_isit_database-master/server/.env > /dev/null << EOT
@@ -195,9 +221,8 @@ def main():
             SQL_HOST=%s
             MONGO_DB=isit_database_mongo
             LOG_DB=log_mongo
-            MONGO_HOST=%s""" % (CONFIG["MYSQL"]["IP"], CONFIG["MONGO"]["IP"])
-    ]
-
+            MONGO_HOST=%s""" % (CONFIG["MYSQL"]["IP"], CONFIG["MONGO"]["IP"])]
+    
     while True:
         test = execute_cmds_ssh(CONFIG["FLASK"]["IP"], "ubuntu", cmds)
         if test == "Failed":
@@ -206,6 +231,7 @@ def main():
         else:
             break
     
+    # Run flask app in background (no hang up)
     while True:
         test = execute_bg(CONFIG["FLASK"]["IP"], "ubuntu", "sudo nohup python3 /50043_isit_database-master/server/app.py < /dev/null > /50043_isit_database-master/server/log.txt 2>&1 &")
         if test == "Failed":
@@ -217,9 +243,13 @@ def main():
     logging.info("MongoDB can be found at %s" % (CONFIG["MONGO"]["IP"]))
     logging.info("MySQL database can be found at %s" % (CONFIG["MYSQL"]["IP"]))
     logging.info("Flask server has started, please visit %s:5000/isit" % (CONFIG["FLASK"]["IP"]))
- 
+    
 
-    # Check if hadoop installed
+    # *=================================================*
+    # *                 HADOOP CONFIG                   *
+    # *=================================================*
+
+    # Check if hadoop/spark installed
     indicator_file_path = "/var/lib/cloud/instances/%s/boot-finished" % (CONFIG["MASTER"]["ID"])
     while True:
         test = exists(indicator_file_path, CONFIG["MASTER"]["IP"], "ubuntu")
@@ -229,7 +259,7 @@ def main():
         else:
             break
 
-    # Call bash script to do config for hadoop/spark
+    # Call bash script to do config for hadoop/spark (set up passwordless ssh, start hdfs and spark processes)
 
     # 2node: keypath, master dns, slave dns, slave ip
     # 4node: keypath, master dns, slave1 dns, slave2 dns, slave3 dns, slave4 dns, slave1 ip, slave2 ip, slave3 ip, slave4 ip
@@ -241,11 +271,6 @@ def main():
 
     print(process_cmd)
     run_command_bash(process_cmd)
-
-    # logging.info("MongoDB can be found at %s" % (CONFIG["MONGO"]["IP"]))
-    # logging.info("MySQL database can be found at %s" % (CONFIG["MYSQL"]["IP"]))
-    # logging.info("Flask server has started, please visit %s:5000/isit" % (CONFIG["FLASK"]["IP"]))
-
 
 
 if __name__ == '__main__':
